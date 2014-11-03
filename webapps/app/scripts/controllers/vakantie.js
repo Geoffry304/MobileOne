d331ba4746cabee597f2c4bfc0eb84ff7c9e9274
Geoffry Van den Eede <geoffry304@live.be>
2014-11-03 21:43:54 +0100

    Vakantiepaginatoevoegen

--- webapps/app/scripts/controllers/vakantie.js
+++ webapps/app/scripts/controllers/vakantie.js
@@ -6,19 +6,15 @@ app.controller('VakantieCtrl', function ($scope, $location, Vakantie) {
 	leeftijdscategorie: "16-17"
 };*/
 $scope.vakanties = Vakantie.all;
-$scope.vakantie = {naam:'', leeftijdscategorie:{van:'',tot:'' }}
+$scope.fotos = {foto1:'', foto2:'', foto3:''};
+$scope.plaats = {plaatsnaam:'', contact:{telNr: '', website: '', adres:{straat:'', nr:'', postcode:'', gemeente:'', long:'', lat:''}}};
+$scope.vakantiePeriode = {vakantieNaam: '', periode:{van:'', tot:''}};
+$scope.vakantie = {naam:'', promoTekst:'', fotos: $scope.fotos , plaats: $scope.plaats, vakantiePeriode: $scope.vakantiePeriode, thema: '', leeftijdscategorie:{van:'', tot:'' }, maxAantalId: '', busVervoer: 'true', eigenVervoer: 'true', fiscaalAftrek: 'true'};
 
 
 $scope.maakVakantie = function () {
-    console.log($scope.vakantie.uid);
     Vakantie.create($scope.vakantie);
-    //.then(function (ref) {
-      //$location.path('/vakanties/' + ref.name());
-    //});
+    
   };
-
-/*$scope.maakVakantie = function() {
-	Vakantie.maakVakantie(vakantie);
-};*/
 
 });
