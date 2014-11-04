19f995769fb7f8506212ed5c9d067d04972383ae
depauwniels <depauwniels@hotmail.com>
2014-11-04 11:59:21 +0100

    kleine aanpassing vakantie

--- webapps/app/scripts/controllers/ToonVakantie.js
+++ webapps/app/scripts/controllers/ToonVakantie.js
@@ -1,5 +1,6 @@
 'use strict';
 
-app.controller('ToonVakantieCtrl', function ($scope, $routeParams, Vakantie) {
+app.controller('ToonVakantieCtrl', function ($scope, $routeParams, Vakantie, Auth) {
   $scope.vakantie = Vakantie.get($routeParams.vakantieId);
+  $scope.signedIn = Auth.signedIn;
 });
