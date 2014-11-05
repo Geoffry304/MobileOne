577e91abab8d1669fc6550fe8ce06955f0bcfb0b
tommeke303 <thomascloostermans@gmail.com>
2014-11-05 20:52:27 +0100

    toevoegen service inschrijving

---
+++ webapps/app/scripts/services/inschrijving.js
@@ -0,0 +1,18 @@
+'use strict';
+/*global app:true*/
+
+app.factory('Inschrijving', function ($firebase, FIREBASE_URL) {
+	var ref = new Firebase(FIREBASE_URL);
+	var inschrijvingen = $firebase(ref.child('inschrijvingen')).$asArray();
+	var Inschrijving = {
+		all: inschrijvingen,
+		create: function (inschrijving){
+			return inschrijvingen.$add(inschrijving).then(function(inschrijvingRef) {
+		        $firebase(ref.child('user_inschrijvingen').child(inschrijving.creatorUID)).$push(inschrijvingRef.name());
+		        return inschrijvingRef;
+					});
+		}
+	};
+});
+
+mksdlqfjmlqdjsf
