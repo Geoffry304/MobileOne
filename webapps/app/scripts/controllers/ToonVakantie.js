9685805cc7af0a770457407f0f3d85184c47bd04
Geoffry Van den Eede <geoffry304@live.be>
2014-11-05 12:19:31 +0100

    Conflict oplossen

--- webapps/app/scripts/controllers/ToonVakantie.js
+++ webapps/app/scripts/controllers/ToonVakantie.js
@@ -1,11 +1,9 @@
 'use strict';
-<<<<<<< HEAD
+
+/*global app:true*/
 
 app.controller('ToonVakantieCtrl', function ($scope, $routeParams, Vakantie, Auth) {
-=======
-/*global app:true*/
-app.controller('ToonVakantieCtrl', function ($scope, $routeParams, Vakantie) {
->>>>>>> origin/master
+
   $scope.vakantie = Vakantie.get($routeParams.vakantieId);
   $scope.signedIn = Auth.signedIn;
 });
