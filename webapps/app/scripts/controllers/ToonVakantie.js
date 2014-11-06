d682c5cc95f268438980dc3c410c276c8a43d2f2
depauwniels <depauwniels@hotmail.com>
2014-11-06 23:49:50 +0100

    Filter op commentsectie

--- webapps/app/scripts/controllers/ToonVakantie.js
+++ webapps/app/scripts/controllers/ToonVakantie.js
@@ -9,9 +9,42 @@ app.controller('ToonVakantieCtrl', function ($scope, $routeParams, Vakantie, Aut
   $scope.comments = Vakantie.getComments($routeParams.vakantieId);
   $scope.comment = {};
   $scope.user = Auth.user;
+  $scope.filter = 'false';
+  $scope.error = 'Gebruik gepaste taal!';
 
     $scope.voegCommentToe = function() {
    	$scope.comment.creator = $scope.user.profile.username;
-  	Vakantie.comment($scope.comment, $routeParams.vakantieId);
+   	if(!filter($scope.comment))
+   	{
+  		Vakantie.comment($scope.comment, $routeParams.vakantieId);
+  		$scope.filter='false';
+  	}	
+  	else
+  	{
+  		$scope.filter='true';
+  	}
   };
+
+  function filter(comment)
+  {
+  	var subcomment = comment.tekst.split(' ');
+  	var notAllowedWords = ['anal','anus','arse','ass','ballsack','balls','bastard','bitch','biatch','bloody','blowjob','bollock','bollok','boner','boob','bugger','bum','butt','buttplug','clitoris','cock','coon','crap','cunt','damn','dick','dildo','dyke','fag','feck','fellate','fellatio','felching','fuck','fudgepacker','fudge packer','flange','Goddamn','hell','homo','jerk','jizz','knobend','labia','lmao','lmfao','muff','nigger','nigga','omg','penis','piss','poop','prick','pube','pussy','queer','scrotum','sex','shit','sh1t','slut','smegma','spunk','tit','tosser','turd','twat','vagina','wank','whore','wtf'];
+  	var index;
+  	var bool;
+  	for(var i = 0; i < subcomment.length; i++)
+  	{
+  		for(var j = 0; j < notAllowedWords.length; j++)
+  		{
+  		index = subcomment[i].localeCompare(notAllowedWords[j]);
+  		if(index === 0)
+  		{
+  			 return true;
+  		}
+  		else{
+  			bool = false;
+  		}
+  	}	
+  	}
+  	return bool;
+  }
 });
