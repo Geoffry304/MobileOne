34261781aafcb4dd66c1f22764780001f5159c54
Geoffry Van den Eede <geoffry304@live.be>
2014-11-10 14:44:20 +0100

    Fb login + profiel aanvullen

--- webapps/app/scripts/controllers/auth.js
+++ webapps/app/scripts/controllers/auth.js
@@ -14,21 +14,12 @@ app.controller('AuthCtrl', function($scope, $location, Auth, user) {
 		});
 	};
 
-	/*$scope.loginWithFacebook = function() {
-		/*Auth.login('facebook').then(function(user) {
-			console.log('Logged in as: ' + user.uid);
-		}, function(error) {
-			console.error('Login failed: ' + error);
+	$scope.loginWithFacebook = function() {
+		Auth.facebookLogin().then(function(){
+			Auth.createFbProfile(Auth.user);
+			$location.path('/');
 		});
-		console.log('click');
-		  Auth.authWithOAuthPopup('facebook', function(error, authData) {
-			if (authData) {
-				console.log(authData.facebook.accessToken);
-			}
-		}, {
-  		scope: 'email,user_likes'
-		});
-	};*/
+	}
 
 	$scope.register = function() {
 		Auth.register($scope.user).then(function(user) {
