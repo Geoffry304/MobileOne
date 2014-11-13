c7e878c7cfd3bf9a83efdb8e445b3225a3eaa6f3
tommeke303 <thomascloostermans@gmail.com>
2014-11-13 19:29:12 +0100

    verder aanvullen van inschrijvingen

--- webapps/app/scripts/controllers/inschrijving.js
+++ webapps/app/scripts/controllers/inschrijving.js
@@ -1,17 +1,41 @@
 'use strict';
 /*global app:true*/
-app.controller('InschrijvingCtrl', function ($scope, $location, Inschrijving, Auth) {
+app.controller('InschrijvingCtrl', function ($scope, $location, Inschrijving, Profile, Auth) {
 	$scope.inschrijving = {aantal: '', naam: '', voornaam: ''};
 	$scope.alerts = [];
 	$scope.signedIn = Auth.signedIn;
 	$scope.logout = Auth.logout;
 	$scope.user = Auth.user;
+	$scope.voegToe = 'Voeg toe';
+	$scope.kinderen = [];
 
 	$scope.submitInschrijving = function ()
 	{
-		$scope.inschrijving.creator = $scope.user.profile.username;
-		$scope.inschrijving.creatorUID = $scope.user.uid;
-		Inschrijving.create($scope.inschrijving).then(function (ref){
+		if ($scope.voegToe === 'Voeg toe') {
+			if ($scope.inschrijving.aantal <= 0 || $scope.inschrijving.aantal >= 11){
+				$scope.alerts = [];
+				$scope.alerts.push({ type: 'error', msg: 'een verkeerd aantal toegevoegd.' });
+			}
+			else{
+				for (var i = 0; i < $scope.inschrijving.aantal; i += 1){
+					$scope.kinderen.push({naam: '', voornaam: ''});
+				}
+				$scope.voegToe = 'inschrijving doorsturen';
+			}
+		}
+		else {
+			$scope.inschrijving.creator = $scope.user.profile.username;
+			$scope.inschrijving.creatorUID = $scope.user.uid;
+			for (var teller = 0; teller < $scope.kinderen.length; teller += 1){
+				$scope.schrijfKindIn(teller);
+			}
+			Inschrijving.create($scope.inschrijving).then(function (ref){
+				$scope.alerts.push({ type: 'success', msg: 'Well done! You successfully read this important alert message.' });
+			});
+		}
+	};
+	$scope.schrijfKindIn = function(teller){
+		Profile.createKind($scope.user.uid, $scope.kinderen[teller]).then(function (ref){
 			$scope.alerts.push({ type: 'success', msg: 'Well done! You successfully read this important alert message.' });
 		});
 	};
