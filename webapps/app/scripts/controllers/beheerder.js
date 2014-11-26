4d997e7a3ce4e242500f9cfabcec32c01444c72e
depauwniels <depauwniels@hotmail.com>
2014-11-26 22:06:29 +0100

    Beheerderspagina tab vakanties

--- webapps/app/scripts/controllers/beheerder.js
+++ webapps/app/scripts/controllers/beheerder.js
@@ -1,8 +1,10 @@
 'use strict';
 /*global app:true*/
-app.controller('BeheerderCtrl', function ($scope, $location, Auth) {
+app.controller('BeheerderCtrl', function ($scope, $location, Auth, Vakantie) {
 	$scope.user = Auth.user;
 	$scope.signedIn = Auth.signedIn;
+	$scope.vakanties = Vakantie.all;
+	$scope.adminvak = 'views/adminVakanties.html';
 	$scope.admin = function(role){
 		return role === '99';
 	};
