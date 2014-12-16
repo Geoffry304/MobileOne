bb567d99ca6ef6e3c9e188020210906a646f3ee3
tommeke303 <thomascloostermans@gmail.com>
2014-12-16 02:08:37 +0100

    aanpassen foto's met nieuw bower project

--- webapps/app/scripts/controllers/ToonVakantie.js
+++ webapps/app/scripts/controllers/ToonVakantie.js
@@ -2,7 +2,7 @@
 
 /*global app:true*/
 
-app.controller('ToonVakantieCtrl', function ($scope, $routeParams, $location, Vakantie, Auth) {
+app.controller('ToonVakantieCtrl', function ($scope, Lightbox,$routeParams, $location, Vakantie, Auth) {
 
   $scope.vakantie = Vakantie.get($routeParams.vakantieId);
   $scope.vakantie.$loaded().then(function(){
@@ -17,6 +17,12 @@ app.controller('ToonVakantieCtrl', function ($scope, $routeParams, $location, Va
   $scope.filter = false;  
   $scope.error = 'Gebruik gepaste taal!';
   $scope.beschikbaarheid = '';
+
+
+  $scope.openLightboxModal = function (index) {
+    Lightbox.openModal($scope.fotos, index);
+  };
+
   $scope.change = function(){
     $location.path('/vakantie/change/' + $routeParams.vakantieId);
   };
